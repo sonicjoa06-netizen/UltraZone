71b549c3f53b54bae99d64c1f3257b341f0acaf7
Joao <sonicjoa06@gmail.com>
2026-06-10 17:11:52 -0300

    feat: Criando model do Usuario e produto

---
+++ UltraZone/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltraZone.Models
+{
+    public class Produto : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UltraZone/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltraZone.Models
+{
+    public class Usuario : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
