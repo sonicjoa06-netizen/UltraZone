cd8abecdf6c902459a0240aa6887da5f7ad8b226
Joao <sonicjoa06@gmail.com>
2026-06-10 16:30:59 -0300

    feat:criando Controller do Usuario e produto

---
+++ UltraZone/Controllers/ProdutosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltraZone.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ UltraZone/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltraZone.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
